bedcae33bffceb07beda2645d71f90a533dd5c0b
taras369 <taras369@gmail.com>
2021-04-11 14:38:01 +0300

    feat: add 4 solution

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,48 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const result = [];
+  for (let i = 0; i < matrix.length; i++) {
+    result[i] = [];
+    for (let j = 0; j < matrix[0].length; j++) {
+      result[i][j] = 0;
+    }
+  }
+  for (let r = 0; r < matrix.length; r++) {
+    for (let c = 0; c < matrix[r].length; c++) {
+      if (matrix[r][c]) {
+        if (result[r][c + 1] !== undefined) {
+          result[r][c + 1] += 1;
+        }
+        if (result[r][c - 1] !== undefined) {
+          result[r][c - 1] += 1;
+        }
+        if (result[r + 1][c] !== undefined) {
+          result[r + 1][c] += 1;
+        }
+        if (result[r + 1][c + 1] !== undefined) {
+          result[r + 1][c + 1] += 1;
+        }
+        if (result[r + 1][c - 1] !== undefined) {
+          result[r + 1][c - 1] += 1;
+        }
+        if (r > 0) {
+          if (result[r - 1][c] !== undefined) {
+            result[r - 1][c] += 1;
+          }
+          if (result[r - 1][c + 1] !== undefined) {
+            result[r - 1][c + 1] += 1;
+          }
+          if (result[r - 1][c - 1] !== undefined) {
+            result[r - 1][c - 1] += 1;
+          }
+        }
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = minesweeper;
+// npm run test ./test/04-mine-sweeper-tests.js
